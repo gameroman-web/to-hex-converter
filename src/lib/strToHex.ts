2d0ce8b3cc60ec1ffd3786cb67cd993bfd10d571
Roman <dev@rman.dev>
2026-01-17 18:35:35 +0000

    Add CI, update dependencies (#2)

--- src/lib/strToHex.ts
+++
@@ -1,8 +0,0 @@
-import { strToNum } from "./strToNum";
-import { numToHex } from "./numToHex";
-
-export function strToHex(str: string, bits: number = 16): string {
-  const num = strToNum(str);
-  const hex = numToHex(num, bits);
-  return hex;
-}
--- src/lib/strToNum.ts
+++
@@ -1,9 +0,0 @@
-export function strToNum(str: string): number {
-  const num = Math.round(parseFloat(str));
-
-  if (isNaN(num)) {
-    throw new Error("Invalid number string");
-  }
-
-  return num;
-}
